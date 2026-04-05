01df15dff5bde05c9445ba5dd1d95791bd85f1c2
Shackw - Kame <kazuki.k@shackw.com>
2026-04-06 00:30:07 +0900

    chore: migrate to webpack build and optimize docker image

--- src/interfaces/features/meta/meta.controller.ts
+++ src/interfaces/features/meta/meta.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, UseFilters, Get } from "@nestjs/common";
+import { Controller, UseFilters, Get, UseGuards } from "@nestjs/common";
 
 import { MetaService } from "@/application/services/meta";
 import { HttpExceptionsFilter } from "@/interfaces/common/filters/http-exception.filter";
+import { AppCheckGuard } from "@/interfaces/common/guards/app-check.guard";
 
 import {
   toGetMetaSummaryResponseDto,
@@ -22,6 +23,7 @@ import type {
 } from "./meta.dto";
 
 @Controller("meta")
+@UseGuards(AppCheckGuard)
 @UseFilters(HttpExceptionsFilter)
 export class MetaController {
   constructor(private readonly meta: MetaService) {}
